c3a12c1866d9acea09d2e11f29c40f59171b47b7
Ricardo Araujo <ricarlojo@gmail.com>
2024-03-29 18:46:28 +0000

    tentativa inicial de passar para o dicionário -> incompleto

--- medicina_proc.py
+++ medicina_proc.py
@@ -38,39 +38,92 @@ doc = re.sub(r'<page number="16"[\w\W]+?<text>20</text>\n',"",doc)
 
 doc = re.sub(r'<page number[\w\W]+?<text>\d+</text>\n',"",doc) # retirar inicio de pagina
 
-doc = re.sub(r"\s+([fma])\n\s*(\w)",r' #\1#\n\2',doc) # retirar espaços e marcar genero
+doc = re.sub(r"\s+([fmas]|sg|sb)\n\s*(\w)",r' #\1#\n\2',doc) # retirar espaços e marcar genero + s(ingular) + sg + sb
 doc = re.sub(r'\s+([fma])\s*pl\n\s*([\w])',r' #\1 pl#\n\2',doc) # caso especial em que aparecia genero e pl (plural)
 doc = re.sub(r"\s\n\s+",r" ", doc) # remoção de um erro de espaçamento
-doc = re.sub(r'<text>\s*(SIN[\w\W]+?)</text>',r'#\1',doc) # marcação de sinónimos
+doc = re.sub(r'<text>\s*(SIN[\w\W]+?)</text>\n',r'#\1\n',doc) # marcação de sinónimos
 doc = re.sub(r'<text>\s*(Vid.+)</text>',r'#\1',doc) # etiquetar VID.
 doc = re.sub(r'(#Vid.-\s\w+\s)\n<text>(.+)</text>',r'\1\2',doc) # lidar com situações que ficou em 2 linhas
-doc = re.sub(r'(b@.+)\n<text><i><b>(.+)</b></i></text>\n.+<b>(.+)</b>.+\n',r'\1\2\3',doc)  # correção para titulos multilinha
+doc = re.sub(r'(b@.+)\n<text><i><b>(.+)</b></i></text>\n.+<b>\s*(.+)</b>.+\n',r'\1\2 #\3#\n',doc)  # correção para titulos multilinha
+doc = re.sub(r'<text><i><b>(.+)</b></i></text>\n#',r'#REL \1\n',doc) # marcação de termos que aparecem a negrito
+doc = re.sub(r'<text><i>(.+)</i></text>\n;',r'\1;',doc) # retirar linhas de ; pós itálico
+doc = re.sub(r'<text><i>(.+)</i></text>',r'\1',doc) # retirar itálico
+doc = re.sub(r'(b@.+)\n.+<b>(.+)</b>.+\n.+<b>.*\s([mfa]).*</b>.+',r'\1\2#\3#',doc) # títulos de 3 linhas e colocar tag no genero
+doc = re.sub(r'(b@.+)\n<text><b>(.+)\s([mfa])(\spl)</b></text>',r'\1\2 #\3\4#',doc) # caso especifico de 2 linhas titulo com gen e pl não marcado
+doc = re.sub(r'(b@.+)\n<text><b>(.+)\s+([mfa])\s*</b></text>\n',r'\1\2#\3#\n',doc)  # tratar titulos com 2 linhas e colocar tag no genero
+doc = re.sub(r'(b@.+)\n<text><b>(.+)\s+([mfa]?)\s*</b></text>\n',r'\1\2#\3#\n',doc) # escrever esta e a linha anterior não estava a funcionar como esperado
 
-#doc = re.sub(r'<text><b>(.+)</b></text>',r'#SUBT \1',doc)
+# Ao analisar elementos com bold reparou-se que havia um subtítulo com 4 linhas, e vários com 2 ou 1.
+""""""
+doc = re.sub(r'<text><b>(.+)</b></text>\n?<text><b>(.+)</b></text>\n?<text><b>(.+)</b></text>\n?<text><b>(.+)</b></text>\n?',r'#SUBT \1\2 \3\4\n',doc)  # não se usou {4} para conseguir aceder aos grupos de captura
+doc = re.sub(r'<text><b>(.+)</b></text>\n?<text><b>(.+)</b></text>\n',r'#SUBT \1\2\n',doc)
+doc = re.sub(r'<text><b>(.+)</b></text>\n',r'#SUBT \1\n',doc)
+
+doc = re.sub(r'.+(VAR.+)</text>',r'#\1',doc) # marcação VAR
+
+doc= re.sub(r'.+(Nota.+)</text>\n',r'#\1\n',doc) # marcação início Notas
+
+doc = re.sub(r'<text>(.+)</text>',r'\1',doc) # retirar tags de texts que faltam
+
+
+
+doc = re.sub(r"(#SUBT   CO)\n@tb@\s(2)",r"\1\2",doc) # caso específico em que CO2 quebra a regra de marcação @tb@
+doc = re.sub(r'(@tb@.+)\n@tb@\s(.+)',r'\1\2',doc)  # erro de tag, duas seguidas referentes ao msm
+doc = re.sub(r'(b@.+)##\n#SUBT\s([mf])\s+\n',r'\1 #\2#\n',doc) # correção de um erro introduzido
+doc = re.sub(r'(b@.+#)#',r'\1Nap#',doc) # marcar os que n tem genero
+
+doc += '@t' # introduzido para fechar o último termo
 
 conceitos = re.findall(r"b@[\w\W]+?@t", doc)
 
+sub_ini = "#SUBT A\n#Vid.- adenina"  # este subtermo está fora de todos os outros. 
 
-# #\n.+?\n
-
-"""
-dicionario = {}
-idiomas = ["@en@","@pt@","@es@","@la@"]
-for elem in conceitos:
-    conceito = {}
-    num, des = re.findall(r'b@\s*(\d+)\s*([\w\W]+)?#',elem)
-    for idioma in idiomas:
-        if idioma in elem:
-
-    dicionario[chave[0]] = elem
-
-"""
-# <text><b>[\w\W]+?</b></text>\n<text>[\w\W]+?</text>\n
-#rest_conceitos = re.findall(r"<text>\s*(\d+)\s</text>\n(.*)",doc)
 file_out = open("medicaProc.txt","w")
 file_out.write(doc)
-#file_out = open("dados.json","w")
-#json.dump(dicionario,file_out)
+file_out.close()
+
+dicionario = {}
+
+for elem in conceitos:
+    tipos = re.search(r'b@.+#\n(.+)',elem).groups()
+    linha = re.search(r'b@\s(\d+)\s(.+)\s#(.+)#',elem) # lista de 1 tuplo
+    num, tit, gen = linha.groups()
+    tit = tit.strip()
+    tit = re.sub(r'\s+',' ', tit)
+    if gen=="Nap":
+        gen = ""
+    dicionario[num] = {"Termo":tit, "Género": gen, "Classes": [tipo for tipo in re.split(r'\s{2,}',tipos[0])]}
+    extras = ["#SIN","#Nota"]
+    for extra in extras:
+        if extra in elem:
+            cont = re.search(r'{}([\w\W]+?)[@#]'.format(extra),elem).groups()
+            extra = extra.strip("#")
+            cont = cont[0].strip("\n")
+            cont = cont.strip(".-")
+            cont = cont.strip()
+            cont = re.sub(r'\s+',' ',cont)
+            dicionario[num][extra] = cont
+    subtemas = ["#SUBT","#Vid"]
+    if "#SUBT" in elem:
+        subt = [sub for sub in re.findall()]
+    
+    
+    trad= re.findall(r"(pt|en|es|la)@([\w\W]+?[@#])",elem)
+    for idioma in trad:
+        cod_pais = idioma[0]
+        tradu = idioma[1].replace("\n"," ")
+        tradu = tradu.replace("@","")
+        tradu= tradu.strip("#")
+        tradu= tradu.strip("@")
+        tradu= tradu.strip()
+        tradu = re.sub(r'\s+',' ', tradu)
+        dicionario[num][cod_pais] = tradu
+
+
+
+
+file_out = open("dados.json","w", encoding="utf-8")
+json.dump(dicionario,file_out,  indent=4, ensure_ascii=False)
 file_out.close()
 
 """
